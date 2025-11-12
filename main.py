88eb26c0486492f69dcadd5d7f6105e9fb196db8
Arseniy <147832892+ArseniyKoz@users.noreply.github.com>
2025-11-12 23:08:46 +0300

    add run by terminal: python main.py /config_name/

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
+import sys
+
 from configs.config_loader import ConfigLoader
 from experiments.experiment_runner import ExperimentConfig, ExperimentRunner
 
 
-def main():
+def main(cfg_name: str):
 
-    config_name = 'experiment/per_dataset.yaml'
-
-    loader = ConfigLoader(config_name)
+    loader = ConfigLoader(cfg_name)
     cfg = loader.load()
+    experiment_config = cfg['experiment_config']
 
-    config = ExperimentConfig()
-    config.clearml_project_name = "SMOTE Test Bench"
-    config.cv_folds = 10
-    config.selected_classifiers = ['LogisticRegression', 'RandomForest', 'SVM']
+    config = ExperimentConfig(experiment_config)
 
     runner = ExperimentRunner(
         config=config,
         create_clearml_task=False,
-        mode='dataset'
     )
 
-    results = runner.per_dataset_experiment(
-        dataset_name=cfg['dataset'],
+    runner.direct_experiments(
         config_name=config_name
     )
 
 
 if __name__ == "__main__":
-    main()
+    config_name = str(sys.argv[1])
+    main(config_name)
